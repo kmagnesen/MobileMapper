5f68ff810b6554efe86bb4f8ba871b70247ec22a
Kyle Magnesen <kylewalkermagnesen@hotmail.com>
2015-01-20 10:38:49 -0600

    Initial Commit

---
+++ MobileMapper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MobileMapper
+//
+//  Created by Kyle Magnesen on 1/20/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
